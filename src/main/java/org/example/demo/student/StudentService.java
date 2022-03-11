615114c124c6c7138cab7c3b7b713a7a78f5d99b
efecdml <efecdml@gmail.com>
2022-03-11 19:56:19 +0300

    Delete mapping, update mapping

--- src/main/java/org/example/demo/student/StudentService.java
+++ src/main/java/org/example/demo/student/StudentService.java
@@ -3,7 +3,9 @@ package org.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -27,5 +29,32 @@ public class StudentService {
         }
         System.out.println(studentModel);
         studentRepository.save(studentModel);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("student with " + studentId + " doesn't exist");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId,
+                              String name,
+                              String email) {
+        StudentModel studentModel = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException("student with " + studentId + " doesn't exist"));
+
+        if (name != null && name.length() > 0 && !Objects.equals(studentModel.getName(), name)) {
+            studentModel.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(studentModel.getEmail(), email)) {
+            Optional<StudentModel> studentModelOptional = studentRepository.findStudentByEmail(email);
+            if (studentModelOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            studentModel.setEmail(email);
+        }
     }
 }
